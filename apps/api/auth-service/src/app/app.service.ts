7dbeeb3774bf57bb67a5374f1f0461111cbec4e1
Ferdi Ardiansa <ferdiardiansa@gmail.com>
2025-12-02 15:54:33 +0700

    refactor: moved db-cli directory, services, infra and api-gateway
    - setup relative paths on pnpm-workspace.yaml
    - change relative paths of tsconfig.base.json on each packages and apps

--- apps/api/auth-service/src/app/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common'
-
-@Injectable()
-export class AppService {
-  generateToken(): { token: string | number } {
-    return { token: 'Bearers: ' + +new Date() }
-  }
-}
